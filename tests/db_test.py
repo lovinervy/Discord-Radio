bc50e96621f3535586bdf08ad2fbce31ebcfcbca
Viner Abubakirov <viner96@yandex.ru>
2022-10-13 16:52:02 +0500

    Update db_tests

--- tests/db_test.py
+++ tests/db_test.py
@@ -21,19 +21,17 @@ RADIO_ACTIVITY = radioActivity(STATION_NAME, GUILD_ID, CHANNEL_ID)
 def setup_db():
     try:
         test_base = Connect(db_path=TEST_DB_PATH)
-        
-        radio = [
-            STATION_NAME, STATION_ADDRESS.url, STATION_ADDRESS.params,
-            SCOREBOARD_ADDRESS.url, SCOREBOARD_ADDRESS.params
-        ]
-        test_base.set_radio(*radio)
-
-        activity = [GUILD_ID, CHANNEL_ID, STATION_NAME]
-        test_base.set_radio_activity(*activity)
-
         yield test_base
     finally:
         os.remove(TEST_DB_PATH)    
+
+
+def test_set_radio(setup_db):
+    radio = [
+        STATION_NAME, STATION_ADDRESS.url, STATION_ADDRESS.params,
+        SCOREBOARD_ADDRESS.url, SCOREBOARD_ADDRESS.params
+    ]
+    setup_db.set_radio(*radio)
 
 
 def test_get_radio_list(setup_db):
@@ -52,6 +50,11 @@ def test_get_radio(setup_db):
     assert setup_db.get_radio(STATION_NAME) == STATION
 
 
+def test_set_radio_activity(setup_db):
+    activity = [GUILD_ID, CHANNEL_ID, STATION_NAME]
+    setup_db.set_radio_activity(*activity)
+
+
 def test_get_radio_activity(setup_db):
     assert setup_db.get_radio_activity() == [RADIO_ACTIVITY,]
 
@@ -62,10 +65,7 @@ def test_delete_radio_activity(setup_db):
 
 
 def test_set_last_scoreboard(setup_db):
-    try:
-        setup_db.set_last_scoreboard(STATION_NAME, DATA_SCOREBOARD1)
-    except:
-        pytest.fail('Test set_last_scoreboard failed')
+    setup_db.set_last_scoreboard(STATION_NAME, DATA_SCOREBOARD1)
 
 
 def test_get_last_scoreboard(setup_db):
@@ -79,3 +79,21 @@ def test_update_last_scoreboard(setup_db):
 
 def test_delete_last_scoreboard(setup_db):
     setup_db.delete_last_scoreboard(STATION_NAME)
+    assert setup_db.get_last_scoreboard(STATION_NAME) == None
+
+
+def test_set_current_scoreboard(setup_db):
+    setup_db.set_current_scoreboard(STATION_NAME, DATA_SCOREBOARD1)
+
+
+def test_get_current_scoreboard(setup_db):
+    setup_db.get_current_scoreboard(STATION_NAME) == DATA_SCOREBOARD1
+
+
+def test_update_current_scoreboard(setup_db):
+    setup_db.update_current_scoreboard(STATION_NAME, DATA_SCOREBOARD2)
+    assert setup_db.get_current_scoreboard(STATION_NAME) == DATA_SCOREBOARD2
+
+def test_delete_current_scoreboard(setup_db):
+    setup_db.delete_current_scoreboard(STATION_NAME)
+    assert setup_db.get_current_scoreboard(STATION_NAME) == None
